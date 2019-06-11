5eaa34ee902485e431c3d52e33e71661eb958106
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2019-06-11 21:07:40 +0200

    sleep between category requests maybe avoid 429 too many requests

--- crawler/get_new_deals.sh
+++ crawler/get_new_deals.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 python3 GetDeals.py 1 2
+sleep 5
 python3 GetDeals.py 2 2
+sleep 5
 python3 GetDeals.py 3 2
+sleep 5
 python3 GetDeals.py 4 2
+sleep 5
 python3 GetDeals.py 5 2
+sleep 5
 python3 GetDeals.py 6 2
+sleep 5
 python3 GetDeals.py 7 2
+sleep 5
 python3 GetDeals.py 8 2
+sleep 5
 python3 GetDeals.py 9 2
+sleep 5
 python3 GetDeals.py 10 2
+sleep 5
 python3 GetDeals.py 11 2
+sleep 5
 python3 GetDeals.py 12 2
+sleep 5
 python3 GetDeals.py 13 2
+sleep 5
 python3 GetDeals.py 14 2
