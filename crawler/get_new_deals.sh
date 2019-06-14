80f3a633dfe0530be85847cd85ceff2a233c6fca
holger.albrecht <holger.albrecht@sovanta.com>
2019-06-14 18:35:43 +0200

    cleaned up gulers fking mess

--- crawler/get_new_deals.sh
+++ crawler/get_new_deals.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-python3 GetDeals.py 1 2
-sleep 5
-python3 GetDeals.py 2 2
-sleep 5
-python3 GetDeals.py 3 2
-sleep 5
-python3 GetDeals.py 4 2
-sleep 5
-python3 GetDeals.py 5 2
-sleep 5
-python3 GetDeals.py 6 2
-sleep 5
-python3 GetDeals.py 7 2
-sleep 5
-python3 GetDeals.py 8 2
-sleep 5
-python3 GetDeals.py 9 2
-sleep 5
-python3 GetDeals.py 10 2
-sleep 5
-python3 GetDeals.py 11 2
-sleep 5
-python3 GetDeals.py 12 2
-sleep 5
-python3 GetDeals.py 13 2
-sleep 5
-python3 GetDeals.py 14 2
+START_TIME=`date +%s`
+
+# Configure the timeframe how old information can be | Options: 2, 12, 24
+TIMEFRAME=2
+# Configure Count of Categories
+CATEGORY_COUNT=14
+# Configure Sleep Time between Categories
+SLEEP_TIME=2
+
+for ((i = 0; i<$CATEGORY_COUNT; i++)) 
+do
+    python3 GetDeals.py $i $TIMEFRAME
+    sleep $SLEEP_TIME
+done
+
+END_TIME=`date +%s`
+RUNTIME=$((END_TIME-START_TIME))
+# prints script runtime
+echo $RUNTIME
