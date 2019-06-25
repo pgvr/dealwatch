fbe5d94654047de0ae729095e2ad45cb8365e04a
Manuel Schurr <admin@manuelschurr.com>
2019-06-25 19:55:27 +0200

    Added MongoDB pipeline, cleaned up code

--- crawler/GetDeals.py
+++ crawler/GetDeals.py
@@ -5,7 +5,7 @@ import sys
 import pymongo
 from pymongo import ReplaceOne
 import json
-import time
+import logging
 
 import credentials
 import datetime
@@ -14,9 +14,11 @@ dbUri = credentials.db["uri"]
 client = pymongo.MongoClient(dbUri)
 geizhalsdb = client.geizhalsdb
 items = geizhalsdb.items
-items.ensure_index("createdAt", expireAfterSeconds=60 * 60 * 24)
+items.create_index("createdAt", expireAfterSeconds=60 * 60 * 24)
 items.create_index([("name", pymongo.TEXT)])
-index = sys.argv[1]
+
+
+category = sys.argv[1]
 hours = sys.argv[2]
 
 categories = {
@@ -40,6 +42,8 @@ categories = {
 
 
 class DealItem(scrapy.Item):
+    _id = scrapy.Field()
+    category = scrapy.Field()
     date = scrapy.Field()
     percent = scrapy.Field()
     name = scrapy.Field()
@@ -48,15 +52,18 @@ class DealItem(scrapy.Item):
     price_old = scrapy.Field()
     seller = scrapy.Field()
     data_from = scrapy.Field()
+    created_at = scrapy.Field()
 
 
 class DealItemLoader(scrapy.loader.ItemLoader):
-    default_input_processor = MapCompose(str.strip)
     default_output_processor = TakeFirst()
 
-    percent_in = MapCompose(lambda v: v[:-1])
-    price_new_in = MapCompose(lambda v: v[2:])
-    price_old_in = MapCompose(lambda v: v[2:])
+    _id_in = Identity()
+    category_in = MapCompose(lambda v: int(v))
+    date_in = MapCompose(lambda v: datetime.datetime.strptime(v, "%d.%m.%Y, %H:%M"))
+    percent_in = MapCompose(lambda v: float(v[:-1].replace(",", ".")))
+    price_new_in = MapCompose(lambda v: float(v[2:].replace(",", ".")))
+    price_old_in = MapCompose(lambda v: float(v[2:].replace(",", ".")))
     seller_in = MapCompose(lambda v: v[6:])
     link_in = MapCompose(lambda v: "https://www.geizhals.de/" + v)
     data_from_in = Identity()
@@ -67,21 +74,48 @@ class JsonPipeline(object):
         self.file = open("items.json", "w")
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
+        line = json.dumps(dict(item)) + ",\n"
         self.file.write(line)
         return item
 
 
-# TODO: Write MongoPipeline
 class MongoPipeline(object):
-    pass
+    collection_name = "items"
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get("MONGO_URI"),
+            mongo_db=crawler.settings.get("MONGO_DATABASE"),
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+        self.items = self.db.items
+        self.items.create_index("createdAt", expireAfterSeconds=60 * 60 * 24)
+        self.items.create_index([("name", pymongo.TEXT)])
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].replace_one(
+            {"_id": item["_id"]}, dict(item), upsert=True
+        )
+        return item
 
 
-class Spider(scrapy.Spider):
+class DealSpider(scrapy.Spider):
 
     name = "Deal_Spider"
     start_urls = [
-        "https://geizhals.de/?bpnew=" + str(hours) + "&thres=20&kats=" + str(index)
+        "https://geizhals.de/?bpnew=" + str(hours) + "&thres=20&kats=" + str(category)
     ]
 
     def parse(self, response):
@@ -110,6 +144,8 @@ class Spider(scrapy.Spider):
 
         for i in range(0, len(output["date"]) - 1):
             loader = DealItemLoader(item=DealItem())
+            loader.add_value("_id", output["link"][i])
+            loader.add_value("category", category)
             loader.add_value("date", output["date"][i])
             loader.add_value("percent", output["percent"][i])
             loader.add_value("name", output["name"][i])
@@ -117,42 +153,22 @@ class Spider(scrapy.Spider):
             loader.add_value("price_new", output["price_new"][i])
             loader.add_value("price_old", output["price_old"][i])
             loader.add_value("seller", output["seller"][i])
-            loader.add_value("data_from", output["data_from"][0])
+            # loader.add_value("data_from", output["data_from"][0])
+            loader.add_value("created_at", datetime.datetime.utcnow())
             yield loader.load_item()
-
-        # bulk_ops = []
-        # for i in range(1, len(output["date"])):
-        #     # generate "unique" id to avoid inserting the same deal multiple times
-        #     # _id must be unique in mongo
-        #     # createdAt is also an index with a TTL of 24 hours from the utc_timestamp
-        #     utc_timestamp = datetime.datetime.utcnow()
-        #     itemDateString = output["date"][i - 1]
-        #     dateobj = datetime.datetime.strptime(itemDateString, "%d.%m.%Y, %H:%M")
-        #     obj = {
-        #         "_id": str(output["link"][i - 1]),
-        #         "category": int(index),
-        #         "date": dateobj,
-        #         "percent": float(output["percent"][i - 1].replace(",", ".")),
-        #         "name": output["name"][i - 1],
-        #         "link": output["link"][i - 1],
-        #         "priceNew": float(output["price_new"][i - 1].replace(",", ".")),
-        #         "priceOld": float(output["price_old"][i - 1].replace(",", ".")),
-        #         "seller": output["seller"][i - 1],
-        #         "createdAt": utc_timestamp,
-        #     }
-        #     request = ReplaceOne({"_id": obj["_id"]}, obj, upsert=True)
-        #     bulk_ops.append(request)
-        # items.bulk_write(bulk_ops)
 
 
 settings = scrapy.settings.Settings(
-    {"ITEM_PIPELINES": {"__main__.JsonPipeline": 100}, "LOG_LEVEL": "CRITICAL"}
+    {
+        "ITEM_PIPELINES": {  # "__main__.JsonPipeline": 300,
+            "__main__.MongoPipeline": 100
+        },
+        "LOG_LEVEL": "DEBUG",
+        "MONGO_URI": credentials.db["uri"],
+        "MONGO_DATABASE": "geizhalsdb",
+    }
 )
 
 process = CrawlerProcess(settings)
-process.crawl(Spider)
-a = time.time()
+process.crawl(DealSpider)
 process.start()
-b = time.time()
-print("!!!", b - a)
-
