49ef4be5a95d16ef05584c54c3787f81d8de64fe
Patrick Göler von Ravensburg <mail@pgvr.dev>
2019-07-08 17:54:48 +0200

    provide app wide http client

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 /* Modules */
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppRoutingModule } from "./pages/app-routing.module";
 
@@ -8,12 +9,12 @@ import { AppRoutingModule } from "./pages/app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
-import { DealCardComponent } from './components/deal-card/deal-card.component';
+import { DealCardComponent } from "./components/deal-card/deal-card.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, DealCardComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HeaderComponent, HomeComponent, DealCardComponent],
+    imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
